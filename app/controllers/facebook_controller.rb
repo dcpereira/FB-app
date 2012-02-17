b3a983ff10bb46196e2467eff6a65d8f73ef544a
David Pereira <dpereira@i-neda.com>
2012-02-17 14:26:12 +0000

    check for yml structure

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -7,6 +7,8 @@ class FacebookController < ApplicationController
   
   def index
     @likes_by_category = current_user.likes_by_category
+    @friends = %w[dude cool yay monster truck]
+    # @friends = current_user.get_friends
   end
 
   def login
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User
   def likes_by_category
     @likes_by_category ||= likes.sort_by {|l| l['name']}.group_by {|l| l['category']}.sort
   end
+  
+  def get_friends
+    @frineds ||= @graph.get_connections(uid,"friends").sort
+  end
 end
