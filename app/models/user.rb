5e058bdca712dc3a9ba531b1ffc5bece5a7fd32d
David Pereira <dpereira@i-neda.com>
2012-02-18 21:54:09 +0000

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User
   end
   
   def get_friend_feed friend_id
-    @friend_feed ||= graph.get_connections(friend_id, "Post")
+    @friend_feed ||= graph.get_connections(friend_id, "feed")
      # @graph.get_connections(friend_id, "feed")
     
   end
