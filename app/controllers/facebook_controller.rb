dd83a56ac94ae9b3cea65893bc646dbf363ba9e1
David Pereira <dpereira@i-neda.com>
2012-02-18 22:27:06 +0000

    bug fix

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -16,7 +16,8 @@ class FacebookController < ApplicationController
   
   def fetch_posts
     friend_uid = params[:selected_friend]
-    @friend_feed = current_user.get_friend_feed friend_uid
+    c_user = User.new(@graph, friend_uid)
+    @friend_feed = c_user.get_friend_feed friend_uid
     # @friend_feed = friend_uid  
   end
 
