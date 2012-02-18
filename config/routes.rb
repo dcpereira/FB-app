3c0d2686e015f9796af5dd577c5999aebf787f89
David Pereira <dpereira@i-neda.com>
2012-02-18 10:52:59 +0000

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ FbBase::Application.routes.draw do
   # root :to => "welcome#index"
 
   match 'facebook/:action' => 'facebook', :as => :facebook
-  post 'facebook/fetch_posts/:selected_friend' => 'facebook/fetch_posts'
+  post 'facebook/fetch_posts => 'facebook/fetch_posts'
   root :to => 'facebook#index'
 
   # See how all your routes lay out with "rake routes"
--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,4 +1,4 @@
 	$('#analyze_button').click(function(){
 	var selected_friend= $("#friend_selector option:selected").val();	
-	$.post("/facebook/fetch_posts/", {selected_friend: selected_friend});
+	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 });
