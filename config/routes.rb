aaebf2893215690c4739509d1ef88f72857712f4
David Pereira <dpereira@i-neda.com>
2012-02-18 11:01:31 +0000

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ FbBase::Application.routes.draw do
   match 'facebook/:action' => 'facebook', :as => :facebook
   post 'facebook/fetch_posts' => 'facebook/fetch_posts'
   post 'facebook/fetch_posts/:selected_friend' => 'facebook/fetch_posts'
+  get 'facebook/fetch_posts/:selected_friend' => 'facebook/fetch_posts'
   root :to => 'facebook#index'
 
   # See how all your routes lay out with "rake routes"
