28261877afa10fd1104d2edc744abd9a40c86301
David Pereira <dpereira@i-neda.com>
2012-02-18 19:33:00 +0000

    bug fix

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#analyze_button').click(function(){
-	var selected_friend= $("#friend_selector option:selected").text();	
+	var selected_friend= $("#friend_selector option:selected").val();	
 	alert(selected_friend);
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 	});
