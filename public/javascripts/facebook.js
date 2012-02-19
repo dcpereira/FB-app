aef35806b012ca70bdcd8b9a5e5debcff2f17b9c
David Pereira <dpereira@i-neda.com>
2012-02-19 20:23:30 +0000

    dhjvd

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -5,29 +5,6 @@ $(document).ready(function() {
 	alert(cdd);
 	
 
-	      google.load("visualization", "1", {packages:["corechart"]});
-	      google.setOnLoadCallback(drawChart);
-	      function drawChart() {
-	        var data = new google.visualization.DataTable();
-	        data.addColumn('string', 'Task');
-	        data.addColumn('number', 'Hours per Day');
-	        var pie_data = document.getElementById('chart_data');
-	        data.addRows([
-	          ['Work',    11],
-	          ['Eat',      2],
-	          ['Commute',  2],
-	          ['Watch TV', 2],
-	          ['Sleep',    7]
-	        ]);
-
-	        var options = {
-	          width: 450, height: 300,
-	          title: 'My Daily Activities'
-	        };
-
-	        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-	        chart.draw(data, options);
-	      }
 
 	
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
