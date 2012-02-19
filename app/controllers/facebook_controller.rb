9273a83d14d57fa21da084882d6f675323e80a99
David Pereira <dpereira@i-neda.com>
2012-02-19 11:52:16 +0000

    Fix bug

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -65,7 +65,7 @@ class FacebookController < ApplicationController
     def facebook_auth
       @oauth = Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_SECRET_KEY)
       if fb_user_info = @oauth.get_user_info_from_cookie(request.cookies)
-        @graph = Koala::Facebook::GraphAPI.new(fb_user_info['access_token'])
+        @graph = Koala::Facebook::API.new(fb_user_info['access_token'])
         @user = User.new(@graph, fb_user_info['user_id'])
       end
     end
