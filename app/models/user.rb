a61f46fdf50acf35891578da155b16aa2046531d
David Pereira <dpereira@i-neda.com>
2012-02-19 15:27:58 +0000

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,8 @@ class User
     @uid = uid
   end
 
-  def likes
-    @likes ||= graph.get_connections(uid, 'likes')
-  end
-
-  def likes_by_category
-    @likes_by_category ||= likes.sort_by {|l| l['name']}.group_by {|l| l['category']}.sort
-  end
-  
   def get_friends
     @friends ||= @graph.get_connections(uid,'friends')
   end
-  
-  def get_friend_feed friend_id
-    # @friend_feed = []
-    # @feed ||= graph.get_connections(friend_id, "feed")   
-  end
+
 end
