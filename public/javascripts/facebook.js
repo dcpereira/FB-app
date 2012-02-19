83e622b46c1445cfa8d434c8429ab8fed0c9a7a5
David Pereira <dpereira@i-neda.com>
2012-02-19 18:17:18 +0000

    gggg

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,24 +1,7 @@
 $(document).ready(function() {
 	$('#analyze_button').click(function(){
 	var selected_friend= $("#friend_selector option:selected").val();	
-	$.post("/facebook/fetch_posts", {selected_friend: selected_friend}, function(chart_data) {
-		google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawChart);
-    function drawChart(chart_data) {
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Task');
-      data.addColumn('number', 'Hours per Day');
-      data.addRows(chart_data);
-
-      var options = {
-        width: 450, height: 300,
-        title: 'MMy Top Commenters'
-      };
-
-      var chart = new google.visualization.PieChart(document.getElementById('results'));
-      chart.draw(data, options);
-    }
-		});
+	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 	});
 });
 
