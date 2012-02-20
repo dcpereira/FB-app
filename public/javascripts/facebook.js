9eb24f4601a3979077378cb15162a7cd85c57e76
David Pereira <dpereira@i-neda.com>
2012-02-20 11:41:30 +0000

    hdhd

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$('#friend_selector').change(function(){
 	var selected_friend= $("#friend_selector option:selected").val();	
-	var cdd= $("#chart_data").val();
-	$('#wait').add('<p>Ranking wall - One moment please...</p>');
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 	
 	
