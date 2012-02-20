59822c68e464fe2d69f611b8c1af71a0cb870715
David Pereira <dpereira@i-neda.com>
2012-02-20 13:28:00 +0000

    gdgdgd

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('#friend_selector').change(function(){
 	alert('works');
 	$('#in_p').show();
+	$('#posts_content').hide();
 	var selected_friend= $("#friend_selector option:selected").val();	
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 	
