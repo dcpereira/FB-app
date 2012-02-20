d95276bb0e54f8e65339932b405b5f7c5ecafdcb
David Pereira <dpereira@i-neda.com>
2012-02-20 12:58:41 +0000

    hhfhf

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 	$('#friend_selector').change(function(){
-		
-		    REDIPS.dialog.hide('undefined');
-		    REDIPS.dialog.show(150, 120, 'Simple dialog');
-		    setTimeout(myHide, 5000);
-		
+	alert('works');
 	var selected_friend= $("#friend_selector option:selected").val();	
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
 	
