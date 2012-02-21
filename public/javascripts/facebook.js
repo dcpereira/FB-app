2df8d711f781d0b0ebb58d38e8f726faf74cebe5
David Pereira <dpereira@i-neda.com>
2012-02-21 18:37:00 +0000

    hdhdhd

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	$('#in_p').show();
 	$('#posts_content').hide();
 	var selected_friend= $("#friend_selector option:selected").val();	
-	var req =	$.post("/facebook/fetch_posts", {selected_friend: selected_friend});
-		req.error(function() {
-			alert("Oops! - Looks like we're experiencing some connection problems - Sorry. Please try again later!");
-					}); 
+	$.post("/facebook/fetch_posts", {selected_friend: selected_friend})
+	.success(function() { alert("second success"); })
+    .error(function() { alert("error"); })
+    .complete(function() { alert("complete"); });
 	});
 });
