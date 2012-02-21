83b584047e831ee11c4eb94ba84bf0ffc8e07474
David Pereira <dpereira@i-neda.com>
2012-02-21 21:17:50 +0000

    hfhfhf

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -13,6 +13,6 @@ module FacebookHelper
     @friends.each do |f|
       friends << [f['name'], f['id']]
     end 
-      select_tag 'friend', options_for_select(friends.sort), {:id => 'friend_selector'}
+      select_tag 'friend', options_for_select(friends.sort),:prompt => "--- Select ---", {:id => 'friend_selector'}
   end
 end
