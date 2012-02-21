f47b58bd0682049b801daddb6d0682f88d584632
David Pereira <dpereira@i-neda.com>
2012-02-21 19:32:59 +0000

    hdhdh

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('#in_p').show();
 	$('#posts_content').hide();
 	var selected_friend= $("#friend_selector option:selected").val();	
-	var selected_friend= $("#friend_selector option:selected").text();	
+	var friend_name = $("#friend_selector option:selected").text();	
 	$.post("/facebook/fetch_posts", {selected_friend: selected_friend, friend_name: friend_name})
 	    .error(function() { 
 				alert("Request Error - There seems to be a connection error. Please try again later."); 
