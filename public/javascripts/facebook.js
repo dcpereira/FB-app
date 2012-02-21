c0b8bf22707fa9f396f81553d1e61998f8fb2efa
David Pereira <dpereira@i-neda.com>
2012-02-21 18:53:30 +0000

    hdhdhd

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -3,12 +3,7 @@ $(document).ready(function() {
 	$('#in_p').show();
 	$('#posts_content').hide();
 	var selected_friend= $("#friend_selector option:selected").val();	
-	$.post("/facebook/fetch_posts", {selected_friend: selected_friend},
-	function() {
-	      alert("success");
-	    })
-	    .success(function() { alert("second success"); })
+	$.post("/facebook/fetch_posts", {selected_friend: selected_friend})
 	    .error(function() { alert("error"); })
-	    .complete(function() { alert("complete"); });
 	});
 });
