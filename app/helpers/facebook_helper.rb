4cce1fdf82c64d4809f5ea4df3e7e3bc515d1d7b
David Pereira <dpereira@i-neda.com>
2012-02-21 23:06:35 +0000

    final final final

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -2,7 +2,7 @@ module FacebookHelper
   def facebook_login_button(size='large')
     content_tag("fb:login-button", nil , {
       :scope => 'user_likes, friends_likes, read_stream',
-      :id => "fb_button_out",
+      :id => "fb_login",
       :autologoutlink => 'true',
       :size => size,
       :onlogin => 'location = "/"'})
